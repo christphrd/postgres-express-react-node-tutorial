2fc65ca626c3acbccfb728a3392642fab1b016d1
github username <diep.christopher@gmail.com>
2018-06-05 16:56:51 -0400

    todo model validation added and has-many association todoItems added

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,10 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING
+    title: DataTypes.STRING,
+    allowNull: false //add validation for db
   }, {});
   Todo.associate = function(models) {
     // associations can be defined here
+    Todo.hasMany(models.TodoItem, {
+      foreignKey: 'todoId',
+      as: 'todoItems', //every query for todo will include todoItems
+    })
   };
   return Todo;
 };
